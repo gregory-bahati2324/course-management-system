17f2668156ff9ee619b10593d372e8ae5b55dafe
Gregory Bahati <gregorybahati8@gmail.com>
2025-08-20 17:00:43 +0300

    Modidify my course service

--- backend/auth_service/app/core/config.py
+++ backend/auth_service/app/core/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     
     SECRET_KEY : str = "super-secret-key"
     
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
     
     class config:
